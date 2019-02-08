48fb6b2fa4059812e3e785f4ac9b8d66f7bed0d0
David <david.rose@perkinelmer.com>
2019-02-08 15:57:55 -0500

    Initial project setup.

---
+++ ScanFieldPoints/PointVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ControlzEx.Standard;
+using Greylock.Common.ViewModels;
+using ReactiveDomain.Bus;
+
+namespace ScanFieldPoints
+{
+    public class PointVM : ViewModel
+    {
+        private Point location;
+
+        public PointVM(IGeneralBus bus) : base(bus)
+        {
+        }
+    }
+}
---
+++ ScanFieldPoints/ScanFieldVM.cs
@@ -0,0 +1,18 @@
+﻿using Greylock.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveDomain.Bus;
+
+namespace ScanFieldPoints
+{
+    public class ScanFieldVM : ViewModel
+    {
+        public ScanFieldVM(IGeneralBus bus) : base(bus)
+        {
+
+        }
+    }
+}
