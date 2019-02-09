6b85d5860ba94c975a30d62886edac38b0aa4b20
David <david.rose@perkinelmer.com>
2019-02-09 11:51:33 -0500

    Added building point list in view model.

--- ScanFieldPoints/ScanFieldVM.cs
+++ ScanFieldPoints/ScanFieldVM.cs
@@ -1,23 +1,72 @@
 ﻿using Greylock.Common.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using ReactiveDomain.Bus;
 using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Windows;
 using ReactiveUI.Legacy;
 
 namespace ScanFieldPoints
 {
     public class ScanFieldVM : ViewModel
     {
+        private static readonly int spacing = 25;
+
         private ReactiveList<Point> _allPoints;
+        private Point _topLeft;
+        private Point _bottomRight;
+        private double height;
+        private double width;
 
         public ScanFieldVM(IGeneralBus bus) : base(bus)
         {
+            Tuple<Point, Point> test = null;
 
+            this.WhenAnyValue(
+                    x => x.TopLeft,
+                    x => x.BottomRight)
+                .Subscribe(RebuildPointList);
+        }
+
+        private void RebuildPointList(Tuple<Point, Point> boundaryPoints)
+        {
+            var topLeft = boundaryPoints.Item1;
+            var bottomRight = boundaryPoints.Item2;
+
+            BuildPointList(topLeft, bottomRight);
+        }
+
+        private void BuildPointList(Point topLeft, Point bottomRight)
+        {
+            var pointList = new List<Point>();
+            var height = bottomRight.Y - topLeft.Y;
+            var width = bottomRight.X - topLeft.X;
+
+            var xPoints = (int)(width / spacing);
+            var yPoints = (int)(height / spacing);
+
+            for (int i = 0; i < xPoints; i++)
+            {
+                for (int j = 0; j < yPoints; j++)
+                {
+                    var p = new Point(i * spacing, j * spacing);
+                    pointList.Add(p);
+                }
+            }
+
+            AllPoints = new ReactiveList<Point>(pointList);
+        }
+
+        public Point TopLeft
+        {
+            get => _topLeft;
+            set => this.RaiseAndSetIfChanged(ref _topLeft, value);
+        }
+
+        public Point BottomRight
+        {
+            get => _bottomRight;
+            set => this.RaiseAndSetIfChanged(ref _bottomRight, value);
         }
 
         public ReactiveList<Point> AllPoints
